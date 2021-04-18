0f2b8ef990ca38158966cb9f4b2917846a171050
quantsr <48443553+quantsr@users.noreply.github.com>
2021-04-18 01:52:41 -0300

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,6 @@
-var it = 0;
+/*Code made by Max Arruda Braz*/
 var openFile = function(event) {
+  oneUploadFlag = true;
   var input = event.target;
 
   var reader = new FileReader();
@@ -8,31 +9,16 @@ var openFile = function(event) {
   window.globalLog = text.split("\n");
   };
   reader.readAsText(input.files[0]);
-  
- 
-  document.getElementById("operation-membership").disabled= false;
+   
   document.getElementById("operation-print").disabled= false;
-  document.getElementById("operation-storage").disabled= false;
-}
-
-var openFile2 = function(event) {
-  var input = event.target;
-
-  var reader = new FileReader();
-  reader.onload = function(){
-  var text = reader.result;
-  window.globalLog2 = text.split("\n");
-  };
-  reader.readAsText(input.files[0]);
-  
- 
   document.getElementById("operation-membership").disabled= false;
-  document.getElementById("operation-print").disabled= false;
   document.getElementById("operation-storage").disabled= false;
+  document.getElementById("operation-treasury").disabled= false;
+  
+  
 }
 
 function readMember(){
-  //first div
   var node = document.getElementById('output');
   if(node.innerText.length > 0){
     node.innerText = "";
@@ -42,47 +28,36 @@ function readMember(){
     if(globalLog[i].includes(',"Membership",')){
       node.innerText += globalLog[i] + "\n";
     }   
-  }         
-  //second div
-   var node2 = document.getElementById('output2');
-  if(node2.innerText.length > 0){
-    node2.innerText = "";
   }
-  
-  for(var i = 0; i < globalLog2.length; i++){
-    if(globalLog2[i].includes('"Membership"')){
-      node2.innerText += globalLog2[i] + "\n";
-    }   
-  } 
 }
 
 function storage(){
-  //first div
   var node = document.getElementById('output');
   if(node.innerText.length > 0){
     node.innerText = "";
   }
   
   for(var i = 0; i < globalLog.length; i++){
-    if(globalLog[i].includes('"Storage"')){
+    if(globalLog[i].includes(',"Storage",')){
       node.innerText += globalLog[i] + "\n";
-    }    
+    }     
   }   
-  //second div
-  var node2 = document.getElementById('output2');
-  if(node2.innerText.length > 0){
-    node2.innerText = "";
+}
+
+function treasury(){
+  var node = document.getElementById('output');
+  if(node.innerText.length > 0){
+    node.innerText = "";
   }
   
-  for(var i = 0; i < globalLog2.length; i++){
-    if(globalLog2[i].includes('"Storage"')){
-      node2.innerText += globalLog2[i] + "\n";
-    }    
+  for(var i = 0; i < globalLog.length; i++){
+    if(globalLog[i].includes(',"Treasury",')){
+      node.innerText += globalLog[i] + "\n";
+    }     
   }   
 }
 
 function print(){
-  //first div
   var node = document.getElementById('output');
   if(node.innerText.length > 0){
     node.innerText = "";
@@ -90,15 +65,4 @@ function print(){
   for(var i = 0; i < globalLog.length; i++){
       node.innerText += globalLog[i] + "\n";     
   }
-  //second div
-  var node2 = document.getElementById('output2');
-  if(node2.innerText.length > 0){
-    node2.innerText = "";
-  }
-  for(var i = 0; i < globalLog2.length; i++){
-      node2.innerText += globalLog2[i] + "\n";     
-  }
 }
-  
-
- 
